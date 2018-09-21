c537a6340ac6dec84d3db496a855af1beab0a085
DrakeEvans <ecg.drake@gmail.com>
2018-09-21 10:29:32 -0400

    added back button and the fileList Object

--- src/FileBrowser.js
+++ src/FileBrowser.js
@@ -1,76 +1,75 @@
 import React, { Component } from "react";
 import FileList from "./FileList";
-const fs = require('fs')
-const os = require('os')
-const path = require('path')
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
 
 export default class FileBrowser extends Component {
   constructor(props) {
     super(props);
-    this.createFileObject = this.createFileObject.bind(this)
-    this.handleClick = this.handleClick.bind(this)
-    this.backButton = this.backButton.bind(this)
+    this.createFileObject = this.createFileObject.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.backButton = this.backButton.bind(this);
+
+    const activeDirectory = this.createFileObject(os.homedir());
+    const fileList = this.buildFileListObject(activeDirectory);
     this.state = {
-      activeDirectory: os.homedir(),
-      fileList: fs.readdirSync(os.homedir()).map( (elem) => {
-        return this.createFileObject(os.homedir(), elem)
-      }
-      )
+      activeDirectory,
+      fileList
     };
   }
 
-  buildFileListObject(parentDir, fileList) { //takes an array of strings
-    output = {};
-    fileList.forEach( (elem) => {
-      output[elem] = this.createFileObject(parentDir, elem)
-    }
-    )
-    return output
+  buildFileListObject(parentObj) {
+    let output = {};
+    fs.readdirSync(parentObj.fullPath).forEach((elem) => {
+      output[elem] = this.createFileObject(parentObj.fullPath, elem);
+    });
+    return output;
   }
 
-  createFileObject(parent, name = null) {
+  createFileObject(parentDir, name = null) {
     if (name) {
-      var fullPath = path.join(parent, name)
+      var fullPath = path.join(parentDir, name);
     } else {
-      var fullPath = parent
-      var name = path.basename(parent)
-      parent = path.dirname(parent)
+      var fullPath = parentDir;
+      var name = path.basename(parentDir);
+      parentDir = path.dirname(parentDir) || null;
     }
-    const isDirectory= fs.statSync(fullPath).isDirectory()
-    
+    const isDirectory = fs.statSync(fullPath).isDirectory();
     return {
       name,
-      parent,
-      fullPath: fullPath,
-      isDirectory,
-      }
-    }
-    
+      parentDir,
+      fullPath,
+      isDirectory
+    };
+  }
+
+  updateActiveDirectory(activeDirectoryObj) {
+    let { fileList } = this.state;
+    const activeDirectory = activeDirectoryObj;
+    fileList = this.buildFileListObject(activeDirectory);
+    this.setState({ activeDirectory, fileList });
+  }
+
   backButton() {
-    const { activeDirectory } = this.state
-    this.setState({activeDirectory: activeDirectory.parent})
-  }  
+    const { activeDirectory } = this.state;
+    this.updateActiveDirectory(
+      this.createFileObject(activeDirectory.parentDir)
+    );
+  }
 
   handleClick(event) {
     event.preventDefault();
-    const fileList = fs.readdirSync(path.join(this.state.activeDirectory, event.target.innerText)).map(
-      (elem) => {
-        return {name: elem }
-      }
-      
-    )
-    this.setState({activeDirectory: event.target.innerText, fileList })
+    this.updateActiveDirectory(this.state.fileList[event.target.innerText]);
   }
-  
 
   render() {
-    console.log('fileList in filebrowswer', this.state.fileList)
     const { activeDirectory, fileList } = this.state;
     return (
       <div className="file-browser">
-      {activeDirectory.name}
-      <button onClick={this.backButton}> Back </button>
-        <FileList fileList={fileList} handleClick = {this.handleClick} />
+        {activeDirectory.name}
+        <button onClick={this.backButton}> Back </button>
+        <FileList fileList={fileList} handleClick={this.handleClick} />
       </div>
     );
   }
